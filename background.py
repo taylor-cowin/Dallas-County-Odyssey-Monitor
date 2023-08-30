cb7f280ebe1a544ad4cf9642cedda6854b2ac601
TaylorCowin <80193327+taylor-cowin@users.noreply.github.com>
2023-08-30 17:35:45 -0500

    debug

--- background.py
+++ background.py
@@ -32,14 +32,14 @@ def check_site():
             else:
                 result = 'DOWN'
                 logger.info("ODYSSEY RETURNED DOWN")
-        except:
-            logger.error("Couldn't get Ody status. Trying again in 5s...")
+        except requests.exceptions.RequestException as e:
+            logger.error("Couldn't get Ody status (error = " + str(e) + "). Trying again in 5s...")
             time.sleep(5)
             http_request()
         
         return result
+    
     result = http_request()
     logger.info(result + " at " + str(run_time))
-    
     #Return a dict with the real results or an error result
     return {"result": result, "run_time": run_time}
