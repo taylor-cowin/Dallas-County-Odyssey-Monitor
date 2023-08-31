aea40c16b8e7e19b501a7d03f627ed065bb81077
TaylorCowin <80193327+taylor-cowin@users.noreply.github.com>
2023-08-30 23:09:20 -0500

    v1 final

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-#package imports
-import threading
-import os
 import logging
-from logging.handlers import RotatingFileHandler
-
+import os
+import threading
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 from time import strftime
+
 from flask import Flask, render_template
 from waitress import serve
 
-
-#local imports
-from background import main_loop
 import mongoconnect
+
+from background import main_loop
 
 #config
 template_dir = os.path.abspath('./static/templates/')
@@ -23,13 +21,13 @@ logger = logging.getLogger('ody_log')
 
 #The main website
 @app.route("/")
+#I legitimately can't remember why these args exist. But the site works and I ain't gonna fight it.
 def index(result=None,last_run_time=None):
     #get the latest entry from the database and breakdown for display
     last_result = mongoconnect.get_latest()
     last_run_time = last_result["run_time"]
     last_run_time = strftime('%I:%M %p on %x', datetime.timetuple(last_run_time))
     result = last_result["result"]
-
     return render_template('index.html', result=result,last_run_time=last_run_time,one_day_uptime=mongoconnect.get_day(), one_week_uptime=mongoconnect.get_week(), one_month_uptime=mongoconnect.get_month(), one_year_uptime=mongoconnect.get_year())
 
 #Starts the web server
@@ -38,31 +36,29 @@ def start_waitress():
         logger.info("Starting web server...")
         serve(app, host='0.0.0.0', port=5000)
         logger.info("Web server started successfully...")
-    except:
-        logger.critical("ERROR: could not start web server.")
+    except Exception as exception:
+        logger.critical("ERROR: Could not start web server. %s", exception)
  
 def init_logger():
-    logger.setLevel(logging.INFO)
     log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
     log_handler = RotatingFileHandler('odychk.log', encoding=None, delay=False, errors=None, maxBytes=1024*25, backupCount=30)
-    log_handler.setLevel(logging.INFO)
     log_handler.setFormatter(log_formatter)
+    logger.setLevel(logging.INFO)
     logger.addHandler(log_handler)
 
 #Launch the background thread to pull info to the db from Odyssey
 def start_update_worker():
-    logger.info("Starting update worker thread...")
+    logger.debug("Starting update worker thread...")
     update_daemon = threading.Thread(group=None, target=main_loop, daemon=True, name='Update Worker')
     try:
         update_daemon.start()
         logger.info("Update worker thread started successfully...")
-    except:
-        logger.info("ERROR: could not start update worker thread...")
+    except Exception as exception:
+        logger.critical("ERROR: could not start update worker thread. %s.", exception)
 
 #Startup calls
 if __name__ == "__main__":
     init_logger()
     start_update_worker()
     start_waitress()
-
-logger.info("Ready to go...")
+    logger.info("Ready to go...")
--- background.py
+++ background.py
@@ -1,47 +1,48 @@
 #Background worker thread that pulls results from Odyssey
-from datetime import datetime
-import requests
-import time
-import mongoconnect
 import logging
+import time
+from datetime import datetime
+
+import requests
+
+import mongoconnect
+
 
 #Check to see if site is up and log result
 def main_loop():
     logger = logging.getLogger('ody_log')
     #on first run, make sure we're starting at or near the start of the minute
-    t = time.strftime('%S', datetime.now().timetuple())
-    time_offset = 60-int(t)
-    logger.info("Time Synchronization: Website checks will begin in " + str(time_offset) + " seconds...")
+    time_now = time.strftime('%S', datetime.now().timetuple())
+    time_offset = 60-int(time_now)
+    logger.debug("Time Synchronization: Website checks will begin in %s seconds...", str(time_offset))
     time.sleep(time_offset)
-    t = time.strftime('%H:%M:%S', datetime.now().timetuple())
-    logger.info("Time synchronization complete. Beginning website check at " + t + "...")
-    
+    time_now = time.strftime('%H:%M:%S', datetime.now().timetuple())
+    logger.debug("Time synchronization complete. Beginning website check at %s...", time_now)
     #The actual loop -- checks site once per minute
     while True:
         try:
             mongoconnect.set_result(check_site())
             time.sleep(60)
-        except:
-            logger.info("Couldn't set result of website check.")
+        except Exception as exception:
+            logger.critical("Couldn't set result of website check. Check database connection. Cause: %s", exception)
+
+#See if the website is up
 def http_request():
-    logger = logging.getLogger('ody_log')
+    #Default to down
     result = 'DOWN'
     try:
+        #App was failing to return due to website failing to timeout -- do not remove timeout clause
         r = requests.get('https://courtsportal.dallascounty.org/DALLASPROD', timeout=10)
         if r.status_code == 200:
             result = 'UP'
-            return result
-    except requests.exceptions.RequestException as e:
-        logger.info(str(e))
+    #This is the most common path -- website is typically 200 or error
+    except requests.exceptions.RequestException:
         pass
-        return result
     return result
 
 def check_site():
     logger = logging.getLogger('ody_log')
     run_time = datetime.utcnow()
-    logger.info("Called check_site() - about to check result. Run_time = " + str(run_time))
     result = http_request()
-    logger.info(result + " at " + str(run_time))
-    #Return a dict with the real results or an error result
+    logger.info("Website is %s at %s", result, str(run_time))
     return {"result": result, "run_time": run_time}
