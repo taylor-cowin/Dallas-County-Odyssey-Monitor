a118c39372da79de6d85a2d19a94248bf655f523
TaylorCowin <80193327+taylor-cowin@users.noreply.github.com>
2023-09-03 20:03:18 -0500

    building installer script

--- odychk.sh
+++ odychk.sh
@@ -25,26 +25,49 @@ restart-background-worker(){
 }
 
 install-services(){
-    if [ -e /etc/systemd/system/odychk-bg.service ]
+    INSTALLBG="/etc/systemd/system/odychk-bg.service"
+    INSTALLWEB="/etc/systemd/system/odychk-web.service"
+    BASEDIR=$(cd $(dirname $0) && pwd)
+    if [ -e $INSTALLBG ]
     then
         echo "Background service already installed... Skipping..."
     else
-        INSTALLBG="/etc/systemd/system/odychk-bg.service"
         echo "Installing background service at $INSTALLBG"
+        echo "[Unit]" > $INSTALLBG
+        echo "Description=OdyChk Background Worker" > $INSTALLBG
+        echo "After=multi-user.target" > $INSTALLBG
+        echo "\n[Service]" > $INSTALLBG
+        echo "WorkingDirectory=$BASEDIR" > $INSTALLBG
+        echo "User=$USER" > $INSTALLBG
+        echo "Restart=always" > $INSTALLBG
+        echo "ExecStart=/usr/bin/python3 $BASEDIR/odychk_worker.py" > $INSTALLBG
+        echo "\n[Install]" > $INSTALLBG
+        echo "WantedBy=multi-user.target" > $INSTALLBG
     fi
 
-    if [ -e /etc/systemd/system/odychk-web.service ]
+    if [ -e $INSTALLWEB ]
     then
         echo "Web service already installed... Skipping..."
     else
-        INSTALLWEB="/etc/systemd/system/odychk-web.service"
         echo "Installing web service at $INSTALLWEB"
+        echo "[Unit]" > $INSTALLWEB
+        echo "Description=OdyChk Flask Web Server" > $INSTALLWEB
+        echo "After=multi-user.target" > $INSTALLWEB
+        echo "\n[Service]" > $INSTALLWEB
+        echo "WorkingDirectory=$BASEDIR" > $INSTALLWEB
+        echo "User=$USER" > $INSTALLWEB
+        echo "Restart=always" > $INSTALLWEB
+        echo "ExecStart=/usr/bin/python3 $BASEDIR/odychk_worker.py" > $INSTALLWEB
+        echo "\n[Install]" > $INSTALLWEB
+        echo "WantedBy=multi-user.target" > $INSTALLWEB
+
     fi
     exit 1
 }
 
 uninstall-services(){
-
+    #PROMPT - ARE YOU SURE?
+    #DELETE FILES ABOVE, IF THEY EXIST
     exit 1
 }
 
@@ -53,7 +76,6 @@ if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root." 1>&2
     exit 1
 fi
-
 
 #NEEDS FIXING https://www.tutorialspoint.com/unix_commands/getopt.htm #
 #Get the flags and act accordingly.
